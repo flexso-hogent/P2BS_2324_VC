f6de6ce4d7486bcb53aca2ff81b44bc79c3f0ab6
Jeremy Crossan <114576701+JeremyCrossan@users.noreply.github.com>
2024-05-06 12:53:03 +0200

    View 'events/sessies aanmaken' aanpassingen

--- Back-end/app/flexso/webapp/controller/EventManager.controller.js
+++ Back-end/app/flexso/webapp/controller/EventManager.controller.js
@@ -1,5 +1,73 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment"
+], function (Controller, MessageToast, JSONModel, Fragment) {
+    "use strict";
+
+    return Controller.extend("flexso.controller.EventManager", {
+        onInit: function () {
+            // Stel een JSON-model in voor sessies als tijdelijke opslag
+            var oSessionModel = new JSONModel({ sessions: [] });
+            this.getView().setModel(oSessionModel, "sessionModel");
+        },
+
+        onNavBack: function () {
+            // Afhandeling van navigatie terug
+            var oHistory = sap.ui.core.routing.History.getInstance();
+            var sPreviousHash = oHistory.getPreviousHash();
+
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1);
+            } else {
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.navTo("home", {}, true );
+            }
+        },
+
+        onAddSession: function () {
+            // Voeg een lege sessie toe aan het sessie-model
+            var oSessionModel = this.getView().getModel("sessionModel");
+            var aSessions = oSessionModel.getProperty("/sessions");
+            aSessions.push({ name: "", type: "", description: "", speaker: "", date: "", room: "" });
+            oSessionModel.setProperty("/sessions", aSessions);
+            
+            MessageToast.show("New session added.");
+        },
+
+        onSaveEvent: function () {
+            
+            var oEventForm = this.getView().byId("eventForm");
+            var oSessionModel = this.getView().getModel("sessionModel");
+            var aSessions = oSessionModel.getProperty("/sessions");
+
+            // Event data
+            var oEventData = {
+                name: oEventForm.byId("_IDGenInput1").getValue(),
+                description: oEventForm.byId("_IDGenInput2").getValue(),
+                date: oEventForm.byId("_IDGenDatePicker1").getValue(),
+                location: oEventForm.byId("_IDGenInput3").getValue(),
+                sessions: aSessions
+            };
+
+            // Communiceer met backend
+            var oODataModel = this.getView().getModel(); 
+            oODataModel.create("/Events", oEventData, {
+                success: function () {
+                    MessageToast.show("Event saved successfully.");
+                },
+                error: function (oError) {
+                    MessageToast.show("Error saving event.");
+                }
+            });
+        }
+    });
+});
+
+/*
+ sap.ui.define([
+    "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "sap/m/MessageToast"
 ], function (Controller, JSONModel, MessageToast) {
@@ -25,6 +93,7 @@ sap.ui.define([
 
             aSessions.push({
                 naam: "",
+                type: "",
                 beschrijving: "",
                 spreker: "",
                 datum: null,
@@ -66,3 +135,4 @@ sap.ui.define([
         }
     });
 });
+*/
